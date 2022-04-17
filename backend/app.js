b7a678a465452db20f2b823ca82c68b66b8a1b2a
2021ankushmukherjee <ankush.mukherjee24@gmail.com>
2022-04-17 10:22:45 +0530

    ecommerce website update

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,9 @@ const express = require("express");
 const errorMiddleware = require("./middleware/error");
 const app = express();
 const cookieParser = require("cookie-parser");
-const upload = require("express-fileupload");
 
 
 
-app.use(express.static("./uploads"));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,11 +13,11 @@ app.use(upload());
 // Route Imports
 const product = require("./routes/productRoutes");
 const user = require("./routes/userRoutes");
-const imageUploads = require("./routes/uploadRoutes");
+
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
-app.use("/static", imageUploads);
+
 
 // middleware for errors
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema({
         url: {
             type: String,
             required: true
-        }
+        }    
     },
     role: {
         type: String,
